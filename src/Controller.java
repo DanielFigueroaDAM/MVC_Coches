0a7a9c57e2daae2e20198164080d0b01cdc4b76b
Figueroa <dfigueroavidal@danielcastelao.org>
2025-04-30 13:35:50 +0200

    adaptando el controller a los metodos staticos

--- src/Controller.java
+++ src/Controller.java
@@ -2,16 +2,14 @@ import java.lang.module.ModuleDescriptor;
 
 public class Controller {
     public static void main(String[] args) {
-        // Instanciamos la vista y el modelo
-        View miView = new View();
-        Model miModel = new Model();
+
 
         // Crear tres coches
-        miModel.crearCoche("LaFerrari", "SBC 1234");
-        miModel.crearCoche("Alpine", "HYU 4567");
-        miModel.crearCoche("Aston Martin", "FGH 3333");
+        Model.crearCoche("LaFerrari", "SBC 1234");
+        Model.crearCoche("Alpine", "HYU 4567");
+        Model.crearCoche("Aston Martin", "FGH 3333");
 
-        Coche ferrari = miModel.getCoche("SBC 1234");
+        Coche ferrari = Model.getCoche("SBC 1234");
         // modifica la velocidad
         int nuevaVelocidad = miModel.cambiarVelocidad("SBC 1234", 30);
 
@@ -24,31 +22,25 @@ public class Controller {
             System.out.println("Error");
         }
         // muestra el menu
-        miView.menu();
+        View.menu();
 
     }
     public static int recibirAumentoVelocidad(String matricula){
-        View miView = new View();
-        Model miModel = new Model();
-        int vAumentada =miModel.subirV(matricula);
+        int vAumentada =Model.subirV(matricula);
         if(vAumentada ==-1) {
             throw new NullPointerException("El coche no existe");
         }
         return vAumentada;
     }
     public static int recibirDisminucionVelocidad(String matricula){
-        View miView = new View();
-        Model miModel = new Model();
-        int vDisminuida =miModel.bajarV(matricula);
+        int vDisminuida =Model.bajarV(matricula);
         if(vDisminuida ==-1) {
             throw new NullPointerException("El coche no existe");
         }
         return vDisminuida;
     }
     public static int recibirVelocidad(String matricula){
-        View miView = new View();
-        Model miModel = new Model();
-        int v = miModel.getVelocidad(matricula);
+        int v = Model.getVelocidad(matricula);
         return v;
     }
 }
