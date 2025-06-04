99a1382c1f13598a3a0c2e0cff7a5ebf96a57222
Figueroa <dfigueroavidal@danielcastelao.org>
2025-06-04 13:16:38 +0200

    empezando el examen del observer

---
+++ src/ObserverGasolina.java
@@ -0,0 +1,7 @@
+public class ObserverGasolina {
+    final static double LIMITE = 10.0;
+    /**
+     * Este es el observador de gasolina
+     * @param coche Coche al que se le actualizó la gasolina
+     */
+}
